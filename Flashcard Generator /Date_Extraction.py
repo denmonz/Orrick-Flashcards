56895e7d3fbcf407db943d5c98fed715c8dd5a2e
NU-1 <algorithm0110+@gmail.com>
2020-01-31 21:38:46 -0600

    Sample entity and date extraction. Install guide in readme

---
+++ Flashcard Generator /Date_Extraction.py
@@ -0,0 +1,60 @@
+import sys
+import lexnlp.nlp.en.segments.sentences as lex_sentences
+import lexnlp.extract.en.dates as lex_dates
+
+# path as command line argument
+# path = sys.argv[1]
+
+# copy&paste brief pdf into .txt file. 
+direct_path = ""
+
+with open(direct_path, 'r') as file:
+    brief = file.read()
+
+processed_brief = lex_sentences.pre_process_document(brief)
+sentences_brief = lex_sentences.get_sentence_list(processed_brief)
+
+#print(sentences_brief)
+
+facts = []
+for sentence in sentences_brief:
+    dates = lex_dates.get_dates(sentence)
+    for date in dates:
+        facts.append((date,sentence))
+
+for fact in facts:
+    print("Question:\nWhy is {} significant?\n\nAnswer:\n{}".format(str(fact[0]), fact[1]))
+    print("\n---------------\n")
+    
+    
+    '''
+    Question:
+    Why is 2018-11-26 significant?
+    Answer:
+    Case: 18-60522 Document: 00514736148 Page: 17 Date Filed: 11/26/2018
+    that these employees were constructively discharged because they faced a Hobson’s Choice?
+    ---------------
+    Question:
+    Why is 2018-11-26 significant?
+    Answer:
+    Case: 18-60522 Document: 00514736148 Page: 18 Date Filed: 11/26/2018
+    performed the task) and assigned dollar values.
+    ---------------
+    Question:
+    Why is 2018-11-26 significant?
+    Answer:
+    Case: 18-60522 Document: 00514736148 Page: 19 Date Filed: 11/26/2018
+    wanted to preserve QPC.
+    ---------------
+    Question:
+    Why is 2010-07-01 significant?
+    Answer:
+    ROA.1083, 2172 (ALJ).1
+    Collective bargaining between DISH and the Union began in July 2010, ROA.2168, and during the first years of bargaining, the parties met approximately a dozen times, for a total of 20 to 25 days.
+    ---------------
+    Question:
+    Why is 2013-03-22 significant?
+    Answer:
+    2 ROA.1083-84, 1089-93; ROA.1410 (Union’s March 22, 2013 proposal, indicating that “[a]ll bargaining unit employees will participate in the same incentive programs as other non-represented DISH network
+    ---------------
+    '''
--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 Firebase deployed website: https://orrick-flashcards.firebaseapp.com
 
 LexNLP:
-    Paper: https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3192101
-    Code: https://github.com/LexPredict/lexpredict-lexnlp
+Paper: https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3192101
+Code: https://github.com/LexPredict/lexpredict-lexnlp
+
+
+
+----------------
+How I got NLTK working on Mac:
+
+download python 3.6
+Use pip3 to install pipenv
+Download LexNLP from https://github.com/LexPredict/lexpredict-lexnlp
+Use pipenv to make a python 3.6 environment in your downloaded LexNLP folder
+use pipenv to install dependencies from python-requirements-full.txt
+manually install any dependencies which pipenv failed to install (for me that was sklearn)
+launch python in your virtual environment
+    in python interactive terminal execute two commands: 
+        import nltk
+        nltk.download( )
+        (This launches an interactive downloader program: I got some connection error: to fix I went to my machine's python 3.6 install location and "$ sudo ./Install\ Certificates.command" )
+    download "all" in the nltk.download(  ) interactive window
