de608948b534796c800a3009c6db5238a16837e9
Taehun Kim <wlehwk115@gmail.com>
2020-02-04 10:45:10 -0600

    quotation mark symbols replaced to UTF-8 supported quotation marks for demo

---
+++ Website/build/precache-manifest.7bc0cdee62b830d158179be5b6fba41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fff2390a58f756cd4086435d955f1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54237f7cff991884d665",
+    "url": "/static/css/main.6aff366b.chunk.css"
+  },
+  {
+    "revision": "1e2934cb5349afde32f0",
+    "url": "/static/js/2.51a6c467.chunk.js"
+  },
+  {
+    "revision": "ef0485bd9e33b680aa2f11772a22e98e",
+    "url": "/static/js/2.51a6c467.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54237f7cff991884d665",
+    "url": "/static/js/main.531d0807.chunk.js"
+  },
+  {
+    "revision": "b5a1360af4dfaafb1558",
+    "url": "/static/js/runtime-main.34e83846.js"
+  }
+]);
