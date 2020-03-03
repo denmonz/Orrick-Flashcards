cc1d8a052ff8d17890c54169fa387a1b13a475c5
Zhili <zhilijwang@gmail.com>
2020-03-03 16:00:58 -0600

    edit ques_filter.py

--- Flashcard Generator/ques_filter.py
+++ Flashcard Generator/ques_filter.py
@@ -7,7 +7,8 @@ def rm_2_short(ques_ls):
 # output string[]: a list of filtered questions 
 # purpose: remove questions that only consist of 2 terms 
 	- questions that short are most likely to be bad
-	- e.g. remove non-sense questions such as e.g. "What 345?" genearted from our system
+	- e.g. remove non-sense questions such as e.g. "What 345?" genearted
+	from our system
 '''
 	for ques in ques_ls:
 		if len(question.split()) <= 2:
@@ -18,12 +19,13 @@ def rm_ques_mark(ques_ls)
 '''
 # input string[]: a python list of questions 
 # output string[]: a list of filtered questions 
-# purpose: remove all question marks of every single question from the input list
-	- complements the "rm_2_short" function: in rare cases there are question marks 
-	  in front of the spaces, which lead to inaccurate lengths of sentences
-	- Zhili has an alternate/improved version of Question-Answering system, which 
-	  performs better if question marks are stripped (we can always add them back
-	  whenever we print the questions) 
+# purpose: remove all question marks of every individual question 
+	 	- complements the "rm_2_short" function: in rare cases there are 
+	 	question marks in front of the spaces, which lead to inaccurate 
+	 	lengths of sentences
+		- our alternate/improved version of Question-Answering 
+		system performs better if question marks are stripped 
+		(we can easily add '?' back whenever we print out the questions) 
 '''
 	for question in question_list:
 		question = question.strip('?')
